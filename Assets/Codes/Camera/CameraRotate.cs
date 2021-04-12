60604f3ba665a717c90fb5deca8b55673d4aeadb
Administrator <Administrator@DESKTOP-2020ECP>
2021-04-12 22:21:40 +0800

    camera rotate

--- Assets/Codes/Camera/CameraRotate.cs
+++ Assets/Codes/Camera/CameraRotate.cs
@@ -3,9 +3,7 @@
 public class CameraRotate : MonoBehaviour
 {
     public float speed = 7.0f;
-
     public Transform player;
-
     private Vector3 offset;
 
     // Start is called before the first frame update
@@ -25,30 +23,36 @@ public class CameraRotate : MonoBehaviour
 
         transform.position = player.position + offset;
 
-        //float mx = Input.GetAxis("Mouse X");
-        //float my =  -Input.GetAxis("Mouse Y");   // 这里为啥要去负
+        /* solution 1 ----------------------- begin
+ 
+        float mx = Input.GetAxis("Mouse X");
+        float my = -Input.GetAxis("Mouse Y");   // 这里为啥要去负
 
-        //if ( Input.GetKey(KeyCode.Mouse1) )
-        //{
-        //    transform.Translate(Vector3.left * speed * mx * Time.deltaTime);
-        //    transform.Translate(Vector3.up * speed * my * Time.deltaTime);
-        //}
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            transform.Translate(Vector3.left * speed * mx * Time.deltaTime);
+            transform.Translate(Vector3.up * speed * my * Time.deltaTime);
+        }
 
-        //if ( Input.GetKey(KeyCode.Mouse0) )
-        //{
-        //    transform.RotateAround(player.transform.position, Vector3.up, mx * speed);
-        //    transform.RotateAround(player.transform.position, transform.right, my * speed);
-        //}
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.RotateAround(player.transform.position, Vector3.up, mx * speed);
+            transform.RotateAround(player.transform.position, transform.right, my * speed);
+        }
+        // solution 1 ----------------------- end  
+        */
 
+        // solution 2 ------------------------- begin
         // button 值为 0 表示左按钮，1 表示右按钮，2 表示中间按钮。按下鼠标按钮时返回 true，释放时返回 false。
         if ( Input.GetMouseButton(0) )
         {
+            Debug.Log("Mouse Button 0");
             Vector3 pos = transform.position;
-            Vector3 rot = transform.eulerAngles; // TODO ??????
+            Vector3 rot = transform.eulerAngles;  // 欧拉角
 
             //围绕原点旋转，也可以将Vector3.zero改为 target.position,就是围绕观察对象旋转
-            transform.RotateAround(Vector3.zero, Vector3.up, Input.GetAxis("Mouse X") * 10);
-            transform.RotateAround(Vector3.zero, Vector3.left, Input.GetAxis("Mouse X") * 10);
+            transform.RotateAround(player.position, Vector3.up, Input.GetAxis("Mouse X") * speed);
+            transform.RotateAround(player.position, Vector3.left, Input.GetAxis("Mouse Y") * speed);
 
             float x = transform.eulerAngles.x;
             float y = transform.eulerAngles.y;
@@ -62,5 +66,8 @@ public class CameraRotate : MonoBehaviour
 
             offset = transform.position - player.position;
         }
+        // Solution 2 -----------------------------------end
     }
 }
+
+
