e0fe175cd7759e560ec72c61975673ae3bd1b5e1
penghuidong <www.1457433217@qq.com>
2021-04-26 00:02:00 +0800

    no networkidentity problem

--- Assets/Codes/Player/Player.cs
+++ Assets/Codes/Player/Player.cs
@@ -7,28 +7,16 @@ public class Player : NetworkBehaviour
 {
     public float speed = 5.0f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("Player Start");
-        var netManager = GetComponent<NetworkManager>();
-        if ( netManager != null )
-        {
-            netManager.playerPrefab = null;
-        }
-        else
-        {
-            Debug.Log("NetworkManager Is Null");
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal"); // AD左右
-        float vertical = Input.GetAxis("Vertical");  //  WS上下
+        if (isLocalPlayer)
+        {
+            float horizontal = Input.GetAxis("Horizontal"); // AD左右
+            float vertical = Input.GetAxis("Vertical");  //  WS上下
 
-        transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
-        transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+            transform.Translate(Vector3.forward * vertical * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * horizontal * speed * Time.deltaTime);
+        }
     }
 }
